5e799860535b81e4e95b3e18450776e2b98ae8e5
XGDragon <s_northman@hotmail.com>
2022-04-06 11:38:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RUCMDataViewer',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/snorthman/RUMCDataViewer/',
     license='MIT',
     author='C.R. Noordman',
